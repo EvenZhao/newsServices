7e6caabe458823faaac7509e65be8cd8f2b521a4
evenzhao <evenzhao@linked-f.com>
2020-08-26 09:37:18 +0800

    feat: 添加接口

--- app/service/save.js
+++ app/service/save.js
@@ -27,23 +27,39 @@ module.exports = (router) => {
 		console.log('文件已被保存');
 	});
 
-	router.get('/savetarget/:id/:savedate/:weight', async (ctx, next) => {
-		const { id, savedate, weight } = ctx.params;
+	router.get('/savetarget/:id/:savedate/:weight/:targetDay', async (ctx, next) => {
+		const { id, savedate, weight, targetDay } = ctx.params;
 		let datas = JSON.parse(fs.readFileSync('targetDatas.json', 'utf8'));
 		let newestData = datas[id];
 		if (newestData) {
-			newestData = {"date": savedate, "weight": weight};
+			newestData = {"date": savedate, "weight": weight, "targetDay":targetDay};
 		} else {
-			datas[id] = {"date": savedate, "weight": weight};
+			datas[id] = {"date": savedate, "weight": weight, "targetDay":targetDay};
 		}
 		fs.writeFile('targetDatas.json', JSON.stringify(datas), (err) => {
 			if (err) throw err;
 			ctx.body = {
 				success: true,
-				list: {"date": savedate, "weight": weight},
+				list: [{"date": savedate, "weight": weight, "targetDay":targetDay}],
 			};
 			console.log('文件已被保存');
 		  });
 		 
+	});
+	router.get('/list/:id', async (ctx, next) => {
+		const { id } = ctx.params;
+		let datas = JSON.parse(fs.readFileSync('targetDatas.json', 'utf8'));
+		let newestData = datas[id];
+		if (newestData) {
+			ctx.body = {
+				success: true,
+				list: [{"date": savedate, "weight": weight, "targetDay":targetDay}],
+			};
+		} else {
+			ctx.body = {
+				success: true,
+				list: [],
+			};
+		}
 	});
 };
